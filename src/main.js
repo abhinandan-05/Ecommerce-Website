27775b61b6d28d26a646f8f1f55e7f85e24bc8da
Abhinandan Kumar <abhicpr05@gmail.com>
2025-07-30 02:28:59 +0530

    add products

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
+import "./style.css";
+
+import products from "/api/products.json";
+
+import { showProductContainer } from "./homeProductCards";
+
+showProductContainer(products);
