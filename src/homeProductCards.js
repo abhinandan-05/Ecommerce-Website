afb8c7fb2b781bfc0260f465f248fa8141396a41
Abhinandan Kumar <abhicpr05@gmail.com>
2025-07-30 12:59:23 +0530

    product card

--- src/homeProductCards.js
+++ src/homeProductCards.js
@@ -13,7 +13,24 @@ export const showProductContainer = (products) => {
 
     const productClone = document.importNode(productTemplate.content, true);
 
+    productClone.querySelector("#cardValue").setAttribute("id", `card${id}`);
+
+    productClone.querySelector(".category").textContent = category;
     productClone.querySelector(".productName").textContent = name;
+    productClone.querySelector(".productImage").src = image;
+    productClone.querySelector(".productImage").alt = name;
+    productClone.querySelector(".productStock").textContent = stock;
+    productClone.querySelector(".productDescription").textContent = description;
+    productClone.querySelector(".productPrice").textContent = `₹${price}`;
+    productClone.querySelector(".productActualPrice").textContent = `₹${
+      price * 4
+    }`;
+
+    const stockElement = productClone.querySelector(".stockElement");
+
+    stockElement.addEventListener("click", (event) => {
+      homeQuantityToggle(event, id, stock);
+    });
 
     productContainer.append(productClone);
   });
